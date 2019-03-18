8cf3f7fa107e8b1752ccedfea9ee84b124dbae77
Ricardo Morais <ricardo.souza.morais@gmail.com>
2019-03-18 19:03:32 -0300

    Fixing the network connection with the usage of docker-compose

--- webapp-redis/index.js
+++ webapp-redis/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const redis = require('redis');
 
+/*
+redis-server - is the name of the service/container declared on docker-compose
+6379 - default Redis port
+*/
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
