354989edb2e5568a4708dc3b63f01312f08ca3f8
Ricardo Morais <ricardo.souza.morais@gmail.com>
2019-03-18 19:27:57 -0300

    Treating the restart policy and also the set of key on the beginning of the app

--- webapp-redis/index.js
+++ webapp-redis/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 /*
 redis-server - is the name of the service/container declared on docker-compose
@@ -10,13 +11,28 @@ const client = redis.createClient({
     host: 'redis-server',
     port: 6379
 });
-client.set('visits', 0);
+
+client.exists('visits', (err, reply) => {
+    if (reply != 1) {
+        client.set('visits', 0);
+    }
+});
 
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
     });
+});
+
+//exiting the application when accessing this route
+app.get('/abend', (req, res) => {
+    /* Exiting with a status code
+     * 0 - exited and everything is OK
+     * 1, 2, 3 etc - exited because something went wrong
+     * With this, docker will decide to restart container or not
+     */
+    process.exit(1);
 });
 
 app.listen(8081, () => {
